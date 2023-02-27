0419e982e1e7a4dac53f92a190f6d20a62a0073f
A14X4Y <leshabelousov2001@gmail.com>
2023-02-27 23:40:38 +0500

    feat(main.js): add getRandomInt, isCorrectLength

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const getRandomInt = (min, max) => {
+  if (min < 0 || max < 0) {
+    throw new RangeError('Числа в диапазоне должны быть положительными');
+  }
+
+  if (min > max) {
+    [min, max] = [max, min];
+  }
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const isCorrectLength = (str, maxLength) => str.length <= maxLength;

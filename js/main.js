8d85cdb3e9279443a5ce9eb0f88751fd8dceaf2d
A14X4Y <leshabelousov2001@gmail.com>
2023-04-23 17:27:41 +0500

    feat: add filter and scale

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 import { generatePosts } from './data.js';
 import { renderThumbnail } from './thumbnail.js';
 import { showUploadFileForm } from './form.js';
-import './form.js';
+// import './form-picture-filters.js';
 renderThumbnail(generatePosts(25));
 document
   .querySelector('#upload-file')
